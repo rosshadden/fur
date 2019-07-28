19dc09a766d068fc6d7913d162e12b071dcc9b4e
Ross Hadden <rosshadden@gmail.com>
2019-07-28 14:01:50 -0400

    Changed registry into a modules directory supporting arbitrary json files.
    To be moved to a common XDG directory later.
    
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- bin/fur.ts
+++ bin/fur.ts
@@ -1,43 +1,61 @@
 #!/usr/bin/env node
 
+const fs = require('fs').promises;
+const path = require('path');
 const pkg = require('../package.json');
 const proc = require('child_process');
-const registry = require('../registry.json');
 
-const argv = require('yargs')
-	.usage('Usage: $0 <cmd>')
-	.alias('h', 'help')
-	.alias('v', 'version')
-	.command(
-		'get <id>',
-		'Get one result from an input list for a given <id>, typically via user input through a fuzzy finder.',
-		{
-			d: {
-					alias: 'default',
+const modules = {};
+
+async function init() {
+	const modulePath = path.join(__dirname, '../modules');
+	const moduleFiles = await fs.readdir(modulePath);
+
+	for (const file of moduleFiles) {
+		modules[path.basename(file, '.json')] = require(path.join(modulePath, file));
+	}
+}
+
+async function main() {
+	await init();
+
+	const argv = require('yargs')
+		.usage('Usage: $0 <cmd>')
+		.alias('h', 'help')
+		.alias('v', 'version')
+		.command(
+			'get <id>',
+			'Get one result from an input list for a given <id>, typically via user input through a fuzzy finder.',
+			{
+				d: {
+						alias: 'default',
+				},
+				f: {
+						alias: 'finder',
+						default: 'fzf',
+				},
+				p: {
+						alias: 'prompt',
+				},
 			},
-			f: {
-					alias: 'finder',
-					default: 'fzf',
+			async (argv) => {
+				const [ group, id ] = argv.id.split(':');
+				if (!modules[group]) throw new Error(`Group not found: ${group}`);
+				if (!modules[group][id]) throw new Error(`ID not found in group: ${id}`);
+				const cmd = modules[group][id];
+
+				const list = proc.exec(cmd);
+				const finder = proc.spawn(argv.finder, [], { stdio: [ 'pipe', 'pipe', 'inherit' ] });
+
+				list.stdout.pipe(finder.stdin);
+
+				for await (const data of finder.stdout) {
+					const output = data.toString().trim()
+					console.log(output);
+				}
 			},
-			p: {
-					alias: 'prompt',
-			},
-		},
-		async (argv) => {
-			const [ group, id ] = argv.id.split(':');
-			if (!registry[group]) throw new Error(`Group not found: ${group}`);
-			if (!registry[group][id]) throw new Error(`ID not found in group: ${id}`);
-			const cmd = registry[group][id];
+		)
+	.argv;
+}
 
-			const list = proc.exec(cmd);
-			const finder = proc.spawn(argv.finder, [], { stdio: [ 'pipe', 'pipe', 'inherit' ] });
-
-			list.stdout.pipe(finder.stdin);
-
-			for await (const data of finder.stdout) {
-				const output = data.toString().trim()
-				console.log(output);
-			}
-		},
-	)
-.argv;
+main();
